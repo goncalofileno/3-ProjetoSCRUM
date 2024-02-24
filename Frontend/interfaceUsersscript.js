be6d04846f11bed4ad9d33a696bf1c2c916a6a3a
ricardocarvalho <ricardoddecarvalho@gmail.com>
2024-02-24 23:11:17 +0000

    Botões visiveis apenas para PO.
    Front end mostra os botoes dependendo do role.

--- Frontend/interfaceUsersscript.js
+++ Frontend/interfaceUsersscript.js
@@ -1,4 +1,23 @@
-document.getElementById("addTaskButton").addEventListener("click", getUsers());
+// dev = Developer
+// sm = Scrum Master
+// po = Product Owner
+
+//o id é o taskButton para usar o mesmo css
+const addUserButton = document.getElementById("addUserButton");
+addUserButton.style.display = "none";
+
+window.onload = function () {
+  getUsers();
+  if (localStorage.getItem("token") == null) {
+    window.location.href = "http://localhost:8080/demo-1.0-SNAPSHOT/";
+    //se o user é Product Owner, o botão de adicionar user é mostrado.
+  } else if (localStorage.getItem("role") == "po") {
+    addUserButton.style.display = "block";
+    addUserButton.addEventListener("click", function () {
+      window.location.href = "register.html";
+    });
+  }
+};
 
 function generateTable(data) {
   let table = "<table>";
@@ -20,25 +39,6 @@ function generateTable(data) {
   return table;
 }
 
-let rows = document.getElementsByTagName("tr");
-Array.from(rows).forEach((row) => {
-  row.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
-    console.log("Right click");
-
-    contextMenu.style.top = `${e.pageY}px`;
-    contextMenu.style.left = `${e.pageX}px`;
-    //Guarda o identificador e a prioridade da tarefa
-    //contextMenu.setAttribute("data-task-id", task.id);
-
-    //Guarda o identificador da tarefa no sessionStorage
-    //sessionStorage.setItem("taskID", task.id);
-
-    //Mostra o popup menu
-    contextMenu.style.display = "block";
-  });
-});
-
 function getUsers() {
   fetch("http://localhost:8080/demo-1.0-SNAPSHOT/rest/user/all", {
     method: "GET",
@@ -58,6 +58,67 @@ function getUsers() {
     });
 }
 
-window.onload = function () {
-  getUsers();
-};
+let rows = document.getElementsByTagName("tr");
+Array.from(rows).forEach((row) => {
+  row.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    console.log("Right click");
+
+    contextMenu.style.top = `${e.pageY}px`;
+    contextMenu.style.left = `${e.pageX}px`;
+    //Guarda o identificador e a prioridade da tarefa
+    //contextMenu.setAttribute("data-task-id", task.id);
+
+    //Guarda o identificador da tarefa no sessionStorage
+    //sessionStorage.setItem("taskID", task.id);
+
+    //Mostra o popup menu
+    contextMenu.style.display = "block";
+  });
+});
+
+async function addUser(user) {
+  const response = await fetch(
+    "http://localhost:8080/demo-1.0-SNAPSHOT/rest/user/add",
+    {
+      method: "POST",
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        token: localStorage.getItem("token"),
+        role: roleChoice,
+      },
+      body: JSON.stringify(user),
+    }
+  );
+
+  const data = await response.json();
+
+  if (response.status === 200 && data.message === "A new user is created") {
+    alert("User is added successfully :)");
+    window.location.href = "index.html";
+  } else {
+    alert(data.message); // show the error message
+    if (response.status === 400) {
+      if (data.message === "One or more parameters are null or blank") {
+        // clear all fields
+        document.getElementById("username").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("photoURL").value = "";
+      } else if (data.message === "Invalid email format") {
+        document.getElementById("email").value = "";
+      } else if (data.message === "Invalid phone number format") {
+        document.getElementById("phone").value = "";
+      } else if (data.message === "Invalid URL format") {
+        document.getElementById("photoURL").value = "";
+      }
+    } else if (
+      response.status === 409 &&
+      data.message === "Invalid Username or Email"
+    ) {
+      document.getElementById("username").value = "";
+      document.getElementById("email").value = "";
+    }
+  }
+}
--- Frontend/register.js
+++ Frontend/register.js
@@ -2,14 +2,27 @@ window.onload = function () {
   const photoPreview = document.getElementById("photoPreviewRegister");
   // Set the initial image source
   photoPreview.src = "https://cdn-icons-png.flaticon.com/512/6388/6388000.png";
+
+  // Check if the user is logged in and is a Product Owner
+  if (
+    localStorage.getItem("token") != null &&
+    localStorage.getItem("role") == "po"
+  ) {
+    // If the user is a Product Owner, show the "Add User" button
+    Array.from(document.getElementsByClassName("ownerOption")).forEach(
+      (element) => {
+        element.style.display = "block";
+      }
+    );
+  }
 };
 
 // Get the photo URL input field and the image
-const photoInput = document.getElementById('photo');
-const photoPreview = document.getElementById('photoPreviewRegister');
+const photoInput = document.getElementById("photo");
+const photoPreview = document.getElementById("photoPreviewRegister");
 
 // Listen for changes to the photo URL input field
-photoInput.addEventListener('input', function() {
+photoInput.addEventListener("input", function () {
   // When the field's value changes, update the image source
   photoPreview.src = photoInput.value;
 });
@@ -51,14 +64,17 @@ document
   });
 
 async function addUser(user) {
-  const response = await fetch("http://localhost:8080/demo-1.0-SNAPSHOT/rest/user/add", {
-    method: "POST",
-    headers: {
-      Accept: "*/*",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  });
+  const response = await fetch(
+    "http://localhost:8080/demo-1.0-SNAPSHOT/rest/user/add",
+    {
+      method: "POST",
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }
+  );
 
   const data = await response.json();
 
@@ -81,7 +97,10 @@ async function addUser(user) {
       } else if (data.message === "Invalid URL format") {
         document.getElementById("photoURL").value = "";
       }
-    } else if (response.status === 409 && data.message === "Invalid Username or Email") {
+    } else if (
+      response.status === 409 &&
+      data.message === "Invalid Username or Email"
+    ) {
       document.getElementById("username").value = "";
       document.getElementById("email").value = "";
     }
